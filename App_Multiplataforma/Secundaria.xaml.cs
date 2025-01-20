9d8040b1d83c03a9b282a1fa388490a41951472f
D <davidlejo2005.2@gmail.com>
2025-01-20 19:59:50 +0100

    Tercer commit, añado un apartado secundario, el  cual te deja iteractuar más que el principal

---
+++ App_Multiplataforma/Secundaria.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace App_Multiplataforma
+{
+    public partial class secundaria : ContentPage
+    {
+        int count = 0;
+        Random random = new Random();
+        string[] imagenes = { "adam.png", "venom.png", "mantis.png", "bucky.png" };
+        string imagenAhora = string.Empty;
+        string ultimaImagen = string.Empty;
+
+        public secundaria()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            ultimaImagen = imagenAhora;
+
+            string nuevaImagen;
+            do
+            {
+                int index = random.Next(imagenes.Length);
+                nuevaImagen = imagenes[index];
+            } while (nuevaImagen == imagenAhora);
+
+            MiImagen.Source = nuevaImagen;
+            imagenAhora = nuevaImagen;
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void boton2(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(ultimaImagen))
+            {
+                MiImagen.Source = ultimaImagen;
+                imagenAhora = ultimaImagen;
+            }
+            else
+            {
+                DisplayAlert("Error", "No hay imagen anterior para mostrar.", "OK");
+            }
+        }
+    }
+}
