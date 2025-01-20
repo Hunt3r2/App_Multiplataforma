ae377ac0f78f743a5d2aa1058bb03bac3db5a4fe
D <davidlejo2005.2@gmail.com>
2025-01-20 18:15:27 +0100

    Agregar archivos de proyecto.

---
+++ App_Multiplataforma/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace App_Multiplataforma
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        Random random = new Random();
+        string[] images = { "adam.png", "venom.png", "mantis.png", "bucky.png" };
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            // Seleccionar una imagen aleatoria
+            int index = random.Next(images.Length);
+            MyImage.Source = images[index];
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
